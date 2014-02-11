cbbefa8a2b664c6aa762908e30cedc8a245bc42e
sheenrox82 <sheenrox82@yahoo.com>
2014-02-10 19:42:38 -0600

    Okay, now I'm about to upload.

--- sheenrox82/RioV/src/util/Util.java
+++ sheenrox82/RioV/src/util/Util.java
@@ -4,5 +4,5 @@ public class Util
 {
 	public static final String MOD_ID = "RioV";
 	public static final String MOD_NAME = "The Mists of RioV";
-	public static final String VERSION = "v2.2.0pre - Feb. 4th";
+	public static final String VERSION = "v2.2.0pre - Feb. 10th";
 }
