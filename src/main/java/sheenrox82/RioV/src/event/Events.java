c859317cbe74b440557be6fa09cde727955aa64a
xbony2 <xbony22@gmail.com>
2015-03-15 18:46:27 -0400

    made things a lot more comfortable (less crappy setup)

---
+++ src/main/java/sheenrox82/RioV/src/event/Events.java
@@ -0,0 +1,415 @@
+package sheenrox82.RioV.src.event;
+
+import java.util.List;
+
+import net.minecraft.entity.boss.EntityDragon;
+import net.minecraft.entity.monster.EntityBlaze;
+import net.minecraft.entity.passive.EntityHorse;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ChatComponentTranslation;
+import net.minecraftforge.event.ServerChatEvent;
+import net.minecraftforge.event.entity.EntityEvent.EntityConstructing;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.event.entity.living.LivingAttackEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.living.LivingDropsEvent;
+import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.entity.living.LivingSetAttackTargetEvent;
+import net.minecraftforge.event.entity.player.BonemealEvent;
+import sheenrox82.RioV.src.api.base.RioVAPI;
+import sheenrox82.RioV.src.api.util.Color;
+import sheenrox82.RioV.src.api.util.RioVPlayer;
+import sheenrox82.RioV.src.block.BlockRioVSapling;
+import sheenrox82.RioV.src.content.RioVBlocks;
+import sheenrox82.RioV.src.content.RioVItems;
+import sheenrox82.RioV.src.entity.mob.jaerin.EntityAdv;
+import sheenrox82.RioV.src.entity.mob.jaerin.EntityAltruEssence;
+import sheenrox82.RioV.src.entity.mob.jaerin.EntityAngel;
+import sheenrox82.RioV.src.entity.mob.jaerin.EntityGalokin;
+import sheenrox82.RioV.src.entity.mob.jaerin.EntityOrc;
+import sheenrox82.RioV.src.entity.mob.jaerin.EntityWoodElf;
+import sheenrox82.RioV.src.entity.mob.jaerin.boss.EntityAltru;
+import sheenrox82.RioV.src.entity.mob.jaerin.boss.EntityTiTun;
+import sheenrox82.RioV.src.entity.mob.jaerin.boss.EntityWoodElfKing;
+import sheenrox82.RioV.src.entity.mob.raetiin.EntityAunTunBodyguard;
+import sheenrox82.RioV.src.entity.mob.raetiin.EntityAunTunMinion;
+import sheenrox82.RioV.src.entity.mob.raetiin.EntityBloodGhoul;
+import sheenrox82.RioV.src.entity.mob.raetiin.EntityDarkElf;
+import sheenrox82.RioV.src.entity.mob.raetiin.EntityHellhound;
+import sheenrox82.RioV.src.entity.mob.raetiin.EntityShadow;
+import sheenrox82.RioV.src.entity.mob.raetiin.EntitySkeletalHorse;
+import sheenrox82.RioV.src.entity.mob.raetiin.EntitySoverianOfficer;
+import sheenrox82.RioV.src.entity.mob.raetiin.EntityTefGuard;
+import sheenrox82.RioV.src.entity.mob.raetiin.boss.EntityAunTun;
+import sheenrox82.RioV.src.entity.mob.raetiin.boss.EntityDarkEssence;
+import sheenrox82.RioV.src.entity.mob.raetiin.boss.EntityDemonAngel;
+import sheenrox82.RioV.src.entity.mob.raetiin.boss.EntityTef;
+import sheenrox82.RioV.src.entity.mob.raetiin.boss.EntityTerron;
+import sheenrox82.RioV.src.util.Util;
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+
+public class Events
+{
+
+	@SubscribeEvent
+	public void onEntityDrop(LivingDropsEvent event) 
+	{
+		if (event.source.getDamageType().equals("player")) 
+		{
+			if (event.entityLiving instanceof EntityBlaze) 
+			{
+				event.entityLiving.dropItem(RioVItems.onyx, 2);
+			}
+			if (event.entityLiving instanceof EntityDragon) 
+			{
+				event.entityLiving.dropItem(RioVItems.dragonSoul, 40);
+			}
+			if (event.entityLiving instanceof EntityHorse) 
+			{
+				event.entityLiving.dropItem(RioVItems.horseMeat, 2);
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void bonemealUsed(BonemealEvent event)
+	{
+		if(event.world.getBlock(event.x, event.y, event.z) == RioVBlocks.glimmerwoodSapling)
+		{
+			((BlockRioVSapling)RioVBlocks.glimmerwoodSapling).func_149878_d(event.world, event.x, event.y, event.z, event.world.rand);
+		}
+
+		if(event.world.getBlock(event.x, event.y, event.z) == RioVBlocks.cherryBlossomSapling)
+		{
+			((BlockRioVSapling)RioVBlocks.cherryBlossomSapling).func_149878_d(event.world, event.x, event.y, event.z, event.world.rand);
+		}
+
+		if(event.world.getBlock(event.x, event.y, event.z) == RioVBlocks.bloodSapling)
+		{
+			((BlockRioVSapling)RioVBlocks.bloodSapling).func_149878_d(event.world, event.x, event.y, event.z, event.world.rand);
+		}
+
+		if(event.world.getBlock(event.x, event.y, event.z) == RioVBlocks.skywoodSapling)
+		{
+			((BlockRioVSapling)RioVBlocks.skywoodSapling).func_149878_d(event.world, event.x, event.y, event.z, event.world.rand);
+		}
+	}
+
+	@SubscribeEvent
+	public void onEntityConstructing(EntityConstructing event) 
+	{
+		if (event.entity instanceof EntityPlayer) 
+		{
+			if (RioVPlayer.get((EntityPlayer) event.entity) == null)
+			{
+				RioVPlayer.register((EntityPlayer) event.entity);
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onEntityJoinWorld(EntityJoinWorldEvent event) 
+	{
+		if (!event.entity.worldObj.isRemote && event.entity instanceof EntityPlayer) 
+		{
+			EntityPlayer player = (EntityPlayer)event.entity;
+
+			if (RioVPlayer.get(player) != null)
+			{
+				RioVPlayer playerRioV = RioVPlayer.get(player);
+
+				playerRioV.loadProxyData(player);
+				
+				if(playerRioV.getReceivedBook() == false)
+				{
+					player.inventory.addItemStackToInventory(new ItemStack(RioVItems.guideBook));
+					playerRioV.setReceivedBook(true);
+				}
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onLivingDeathEvent(LivingDeathEvent event) 
+	{
+		if (!event.entity.worldObj.isRemote && event.entity instanceof EntityPlayer)
+		{
+			EntityPlayer entityPlayer = (EntityPlayer)event.entity;
+			RioVPlayer player = RioVPlayer.get(entityPlayer);
+			
+			if (player != null)
+			{
+				player.saveProxyData(entityPlayer);
+			}
+		}
+
+		if ((event.source.getEntity() instanceof EntityPlayer))
+		{
+			EntityPlayer entityplayer = (EntityPlayer)event.source.getEntity();
+			RioVPlayer player = RioVPlayer.get(entityplayer);
+
+			if(event.entityLiving instanceof EntityAunTunMinion || event.entityLiving instanceof EntityAunTunBodyguard || event.entityLiving instanceof EntityBloodGhoul || 
+					event.entityLiving instanceof EntityDarkElf || event.entityLiving instanceof EntityHellhound || event.entityLiving instanceof EntityShadow || 
+					event.entityLiving instanceof EntitySkeletalHorse || event.entityLiving instanceof EntitySoverianOfficer || event.entityLiving instanceof EntityTefGuard || 
+					event.entityLiving instanceof EntityAunTun || event.entityLiving instanceof EntityDarkEssence || event.entityLiving instanceof EntityDemonAngel || 
+					event.entityLiving instanceof EntityTef || event.entityLiving instanceof EntityTerron)
+			{
+				player.consumeRep(-3);
+			}
+
+			if(event.entityLiving instanceof EntityAdv || event.entityLiving instanceof EntityAltruEssence || event.entityLiving instanceof EntityAngel || 
+					event.entityLiving instanceof EntityGalokin || event.entityLiving instanceof EntityOrc || event.entityLiving instanceof EntityWoodElf || 
+					event.entityLiving instanceof EntityAltru || event.entityLiving instanceof EntityTiTun || event.entityLiving instanceof EntityWoodElfKing)
+			{
+				player.consumeRep(3);
+			}
+		}
+	}
+
+	public int regenTimer;
+
+	@SubscribeEvent
+	public void playerUpdate(LivingEvent.LivingUpdateEvent event)
+	{
+		if (!event.entity.worldObj.isRemote && event.entity instanceof EntityPlayer) 
+		{
+			EntityPlayer entityplayer = (EntityPlayer)event.entity;
+			RioVPlayer player = RioVPlayer.get(entityplayer);
+
+			if (RioVPlayer.get(entityplayer) != null)
+			{
+				player.saveProxyData(entityplayer);
+			}
+
+			if(player.getCurrentRep() > 0)
+			{
+				player.setFactionID(player.jaerinID);
+			}
+
+			if(player.getCurrentRep() == 0)
+			{
+				player.setFactionID(player.noFactionID);
+			}
+
+			if(player.getCurrentRep() < 0)
+			{
+				player.setFactionID(player.raetiinID);
+			}
+
+			if(!entityplayer.capabilities.isCreativeMode)
+			{
+				if(player.getCurrentEos() < player.getMaxEos())
+				{
+					++regenTimer;
+
+					if(regenTimer > 100)
+					{
+						player.consumeEos(-1);
+						regenTimer = 0;
+					}
+				}
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onPlayerDamage(LivingAttackEvent event)
+	{
+		if (event.entity instanceof EntityPlayer) 
+		{
+			EntityPlayer player = (EntityPlayer)event.entity;
+			RioVPlayer riovPlayer = RioVPlayer.get(player);
+
+			if(event.source.getDamageType().equals("onFire") || event.source.getDamageType().equals("inFire") || event.source.getDamageType().equals("lava"))
+			{
+				if(riovPlayer.getFactionID() == riovPlayer.raetiinID)
+				{
+					event.setCanceled(true);
+				}
+			}
+
+			if(event.source.getDamageType().equals("fall") || event.source.getDamageType().equals("cactus"))
+			{
+				if(riovPlayer.getFactionID() == riovPlayer.jaerinID)
+				{
+					event.setCanceled(true);
+				}
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void entityTarget(LivingSetAttackTargetEvent event)
+	{
+		if (event.target instanceof EntityPlayer) 
+		{
+			EntityPlayer player = (EntityPlayer)event.target;
+			RioVPlayer riov = RioVPlayer.get(player);
+
+			if(riov.getFactionID() == riov.raetiinID)
+			{
+				if(event.entity instanceof EntityAunTunMinion)
+				{
+					((EntityAunTunMinion)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntityAunTunBodyguard)
+				{
+					((EntityAunTunBodyguard)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntityBloodGhoul)
+				{
+					((EntityBloodGhoul)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntityDarkElf)
+				{
+					((EntityDarkElf)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntityHellhound)
+				{
+					((EntityHellhound)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntityShadow)
+				{
+					((EntityShadow)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntitySkeletalHorse)
+				{
+					((EntitySkeletalHorse)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntitySoverianOfficer)
+				{
+					((EntitySoverianOfficer)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntityTefGuard)
+				{
+					((EntityTefGuard)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntityAunTun)
+				{
+					((EntityAunTun)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntityDarkEssence)
+				{
+					((EntityDarkEssence)event.entity).setAttackTarget(null);
+				}
+				if(event.entityLiving instanceof EntityDemonAngel)
+				{
+					((EntityDemonAngel)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntityTef)
+				{
+					((EntityTef)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntityTerron)
+				{
+					((EntityTerron)event.entity).setAttackTarget(null);
+				}
+			}
+
+			if(riov.getFactionID() == riov.jaerinID)
+			{
+				if(event.entity instanceof EntityAdv)
+				{
+					((EntityAdv)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntityAltruEssence)
+				{
+					((EntityAltruEssence)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntityAngel)
+				{
+					((EntityAngel)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntityGalokin)
+				{
+					((EntityGalokin)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntityOrc)
+				{
+					((EntityOrc)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntityWoodElf)
+				{
+					((EntityWoodElf)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntityAltru)
+				{
+					((EntityAltru)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntityTiTun)
+				{
+					((EntityTiTun)event.entity).setAttackTarget(null);
+				}
+				if(event.entity instanceof EntityWoodElfKing)
+				{
+					((EntityWoodElfKing)event.entity).setAttackTarget(null);
+				}
+			}
+		}
+	}
+
+	@SubscribeEvent
+	public void onServerChatReceivedEvent(ServerChatEvent event)
+	{
+		if (event.player != null) 
+		{
+			EntityPlayer player = (EntityPlayer) event.player;
+			RioVPlayer riovPlayer = RioVPlayer.get(player);
+
+			if (player != null)
+			{
+				event.setCanceled(true);
+
+				List<?> players = MinecraftServer.getServer().getConfigurationManager().playerEntityList;
+
+				for (int i = 0; i < + players.size(); i++) 
+				{
+					EntityPlayer target = (EntityPlayer) players.get(i);
+
+					String raetiinMsg = "[" + Color.DARK_RED + "Raetiin" + Color.WHITE + "] <" + player.getDisplayName() + "> " + event.message;
+					String jaerinMsg = "[" + Color.GREEN + "Jaerin" + Color.WHITE + "] <" + player.getDisplayName() + "> " + event.message;
+					String noFacMsg = "[" + "No Faction" + "] <" + player.getDisplayName() + "> " + event.message;
+
+					if(player.getDisplayName().equals(Util.DEVELOPERS[0]))
+					{
+						if(riovPlayer.getFactionID() == 2)
+						{
+							target.addChatMessage(new ChatComponentTranslation(Color.DARK_AQUA + "[" + Color.DARK_GRAY + "RioV" + Color.DARK_AQUA + "]" + Color.WHITE + jaerinMsg));
+						}
+
+						if(riovPlayer.getFactionID() == 0)
+						{
+							target.addChatMessage(new ChatComponentTranslation(Color.DARK_AQUA + "[" + Color.DARK_GRAY + "RioV" + Color.DARK_AQUA + "]" + Color.WHITE + noFacMsg));
+						}
+
+						if(riovPlayer.getFactionID() == 1)
+						{
+							target.addChatMessage(new ChatComponentTranslation(Color.DARK_AQUA + "[" + Color.DARK_GRAY + "RioV" + Color.DARK_AQUA + "]" + Color.WHITE + raetiinMsg));
+						}
+					}
+					else
+					{
+						if(riovPlayer.getFactionID() == 2)
+						{
+							target.addChatMessage(new ChatComponentTranslation(jaerinMsg));
+						}
+
+						if(riovPlayer.getFactionID() == 0)
+						{
+							target.addChatMessage(new ChatComponentTranslation(noFacMsg));
+						}
+
+						if(riovPlayer.getFactionID() == 1)
+						{
+							target.addChatMessage(new ChatComponentTranslation(raetiinMsg));
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ src/main/java/sheenrox82/RioV/src/proxy/ClientProxy.java
@@ -0,0 +1,181 @@
+package sheenrox82.RioV.src.proxy;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.model.ModelBiped;
+import net.minecraft.item.Item;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.MinecraftForgeClient;
+import net.minecraftforge.common.MinecraftForge;
+import sheenrox82.RioV.src.api.base.RioVAPI;
+import sheenrox82.RioV.src.api.util.Color;
+import sheenrox82.RioV.src.content.RioVBlocks;
+import sheenrox82.RioV.src.content.RioVItems;
+import sheenrox82.RioV.src.entity.mob.jaerin.EntityAdv;
+import sheenrox82.RioV.src.entity.mob.jaerin.EntityAltruEssence;
+import sheenrox82.RioV.src.entity.mob.jaerin.EntityAngel;
+import sheenrox82.RioV.src.entity.mob.jaerin.EntityGalokin;
+import sheenrox82.RioV.src.entity.mob.jaerin.EntityOrc;
+import sheenrox82.RioV.src.entity.mob.jaerin.EntityWoodElf;
+import sheenrox82.RioV.src.entity.mob.jaerin.boss.EntityAltru;
+import sheenrox82.RioV.src.entity.mob.jaerin.boss.EntityTiTun;
+import sheenrox82.RioV.src.entity.mob.jaerin.boss.EntityWoodElfKing;
+import sheenrox82.RioV.src.entity.mob.neutral.EntityDove;
+import sheenrox82.RioV.src.entity.mob.neutral.EntityFairy;
+import sheenrox82.RioV.src.entity.mob.neutral.EntityMage;
+import sheenrox82.RioV.src.entity.mob.neutral.EntityNizonian;
+import sheenrox82.RioV.src.entity.mob.neutral.EntityPaladin;
+import sheenrox82.RioV.src.entity.mob.neutral.EntityRabbit;
+import sheenrox82.RioV.src.entity.mob.neutral.EntityVravinite;
+import sheenrox82.RioV.src.entity.mob.raetiin.EntityAunTunBodyguard;
+import sheenrox82.RioV.src.entity.mob.raetiin.EntityAunTunMinion;
+import sheenrox82.RioV.src.entity.mob.raetiin.EntityBloodGhoul;
+import sheenrox82.RioV.src.entity.mob.raetiin.EntityDarkElf;
+import sheenrox82.RioV.src.entity.mob.raetiin.EntityHellhound;
+import sheenrox82.RioV.src.entity.mob.raetiin.EntityShadow;
+import sheenrox82.RioV.src.entity.mob.raetiin.EntityShadowWizard;
+import sheenrox82.RioV.src.entity.mob.raetiin.EntitySkeletalHorse;
+import sheenrox82.RioV.src.entity.mob.raetiin.EntitySoverianOfficer;
+import sheenrox82.RioV.src.entity.mob.raetiin.EntityTefGuard;
+import sheenrox82.RioV.src.entity.mob.raetiin.boss.EntityAunTun;
+import sheenrox82.RioV.src.entity.mob.raetiin.boss.EntityDarkEssence;
+import sheenrox82.RioV.src.entity.mob.raetiin.boss.EntityDemonAngel;
+import sheenrox82.RioV.src.entity.mob.raetiin.boss.EntityTef;
+import sheenrox82.RioV.src.entity.mob.raetiin.boss.EntityTerron;
+import sheenrox82.RioV.src.entity.projectile.EntityDarkMatter;
+import sheenrox82.RioV.src.entity.projectile.EntityDarknessArrow;
+import sheenrox82.RioV.src.entity.projectile.EntityPinkEssence;
+import sheenrox82.RioV.src.entity.projectile.EntityRawDarkMatter;
+import sheenrox82.RioV.src.entity.projectile.EntityVraviniteArrow;
+import sheenrox82.RioV.src.gui.hud.GuiEosBar;
+import sheenrox82.RioV.src.gui.hud.GuiHelmetOverlay;
+import sheenrox82.RioV.src.gui.hud.GuiReputationMeter;
+import sheenrox82.RioV.src.lib.RioVLib;
+import sheenrox82.RioV.src.model.ModelDemonAngel;
+import sheenrox82.RioV.src.model.ModelDragonWings;
+import sheenrox82.RioV.src.model.ModelHellhound;
+import sheenrox82.RioV.src.model.ModelTerron;
+import sheenrox82.RioV.src.render.RenderAltru;
+import sheenrox82.RioV.src.render.RenderAngel;
+import sheenrox82.RioV.src.render.RenderAunTun;
+import sheenrox82.RioV.src.render.RenderBipedCore;
+import sheenrox82.RioV.src.render.RenderBossCore;
+import sheenrox82.RioV.src.render.RenderDarkEssence;
+import sheenrox82.RioV.src.render.RenderDove;
+import sheenrox82.RioV.src.render.RenderEssence;
+import sheenrox82.RioV.src.render.RenderFairy;
+import sheenrox82.RioV.src.render.RenderLivingCore;
+import sheenrox82.RioV.src.render.RenderOrc;
+import sheenrox82.RioV.src.render.RenderPaladin;
+import sheenrox82.RioV.src.render.RenderRabbit;
+import sheenrox82.RioV.src.render.RenderSkeletalHorse;
+import sheenrox82.RioV.src.render.RenderTef;
+import sheenrox82.RioV.src.render.RenderTiTun;
+import sheenrox82.RioV.src.render.RenderWoodElfKing;
+import sheenrox82.RioV.src.render.item.ItemRenderBow;
+import sheenrox82.RioV.src.render.item.ItemRenderJaerinFlag;
+import sheenrox82.RioV.src.render.item.ItemRenderLongbow;
+import sheenrox82.RioV.src.render.item.ItemRenderNoFactionFlag;
+import sheenrox82.RioV.src.render.item.ItemRenderRaetiinFlag;
+import sheenrox82.RioV.src.render.projectile.RenderDarkMatter;
+import sheenrox82.RioV.src.render.projectile.RenderDarknessArrow;
+import sheenrox82.RioV.src.render.projectile.RenderPinkEssence;
+import sheenrox82.RioV.src.render.projectile.RenderVraviniteArrow;
+import sheenrox82.RioV.src.render.tileentity.RenderTileEntityInfuser;
+import sheenrox82.RioV.src.render.tileentity.RenderTileEntityJaerinFlag;
+import sheenrox82.RioV.src.render.tileentity.RenderTileEntityNoFactionFlag;
+import sheenrox82.RioV.src.render.tileentity.RenderTileEntityRaetiinFlag;
+import sheenrox82.RioV.src.tileentity.TileEntityInfuser;
+import sheenrox82.RioV.src.tileentity.TileEntityJaerinFlag;
+import sheenrox82.RioV.src.tileentity.TileEntityNoFactionFlag;
+import sheenrox82.RioV.src.tileentity.TileEntityRaetiinFlag;
+import sheenrox82.RioV.src.util.ExpansionChecker;
+import sheenrox82.RioV.src.util.Util;
+
+import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.client.registry.RenderingRegistry;
+
+public class ClientProxy extends CommonProxy 
+{	
+	private static final ModelDragonWings dragonWings = new ModelDragonWings(1.0F);
+
+	@Override
+	public void init() 
+	{
+		RenderingRegistry.registerEntityRenderingHandler(EntityMage.class, new RenderBipedCore(RioVLib.mage, "Mage", "No Faction"));
+		RenderingRegistry.registerEntityRenderingHandler(EntitySkeletalHorse.class, new RenderSkeletalHorse());
+		RenderingRegistry.registerEntityRenderingHandler(EntityAunTunBodyguard.class, new RenderBipedCore(RioVLib.auntun, "Aun'Tun Bodyguard", Color.DARK_RED + "Raetiin"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityAdv.class, new RenderBipedCore(RioVLib.adventurer, "Adventurer", Color.GREEN + "Jaerin"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityTerron.class, new RenderBossCore(new ModelTerron(), 0.5F, 1.8F));
+		RenderingRegistry.registerEntityRenderingHandler(EntityDemonAngel.class, new RenderBossCore(new ModelDemonAngel(), 0.5F, 2F));
+		RenderingRegistry.registerEntityRenderingHandler(EntityDarkElf.class, new RenderBipedCore(RioVLib.dark_elf, "Dark Elf", Color.DARK_RED + "Raetiin"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityAunTun.class, new RenderAunTun());
+		RenderingRegistry.registerEntityRenderingHandler(EntityAltruEssence.class, new RenderEssence());
+		RenderingRegistry.registerEntityRenderingHandler(EntityNizonian.class, new RenderLivingCore(new ModelTerron(), 0.5F, 1.5F, 1.5F, 1.5F, "Nizonian", "No Faction"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityGalokin.class, new RenderBipedCore(RioVLib.galokin, "Galokin", Color.GREEN + "Jaerin"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityOrc.class, new RenderOrc());
+		RenderingRegistry.registerEntityRenderingHandler(EntityFairy.class, new RenderFairy());
+		RenderingRegistry.registerEntityRenderingHandler(EntityDarkEssence.class, new RenderDarkEssence());
+		RenderingRegistry.registerEntityRenderingHandler(EntityHellhound.class, new RenderLivingCore(new ModelHellhound(), 0.6F, 1.5F, 1.5F, 1.5F, "Hellhound", Color.DARK_RED + "Raetiin"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityWoodElf.class, new RenderBipedCore(RioVLib.wood_elf, "Wood Elf", Color.GREEN + "Jaerin"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityTef.class, new RenderTef());
+		RenderingRegistry.registerEntityRenderingHandler(EntityTefGuard.class, new RenderBipedCore(RioVLib.essence, "Tef Guard", Color.DARK_RED + "Raetiin"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityShadow.class, new RenderBipedCore(RioVLib.shadow, (String)null, (String)null));
+		RenderingRegistry.registerEntityRenderingHandler(EntityVravinite.class, new RenderBipedCore(RioVLib.vravinite, "Vravinite", "No Faction"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityDove.class, new RenderDove());
+		RenderingRegistry.registerEntityRenderingHandler(EntityAngel.class, new RenderAngel());
+		RenderingRegistry.registerEntityRenderingHandler(EntityRabbit.class, new RenderRabbit());
+		RenderingRegistry.registerEntityRenderingHandler(EntitySoverianOfficer.class, new RenderBipedCore(RioVLib.officer, "Soverian Officer", Color.DARK_RED + "Raetiin"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityPaladin.class, new RenderPaladin());
+		RenderingRegistry.registerEntityRenderingHandler(EntityPinkEssence.class, new RenderPinkEssence(new ResourceLocation(Util.MOD_ID + ":" + "pinkEssence.png")));
+		RenderingRegistry.registerEntityRenderingHandler(EntityDarkMatter.class, new RenderDarkMatter(new ResourceLocation(Util.MOD_ID + ":" + "darkMatter.png")));
+		RenderingRegistry.registerEntityRenderingHandler(EntityAunTunMinion.class, new RenderBipedCore(RioVLib.auntun, "Aun'Tun Minion", Color.DARK_RED + "Raetiin"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityBloodGhoul.class, new RenderBipedCore(RioVLib.blood_ghoul, "Blood Ghoul", Color.DARK_RED + "Raetiin"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityDarknessArrow.class, new RenderDarknessArrow(RioVLib.darkness_arrow));
+		RenderingRegistry.registerEntityRenderingHandler(EntityVraviniteArrow.class, new RenderVraviniteArrow(RioVLib.vravinite_arrow));
+		RenderingRegistry.registerEntityRenderingHandler(EntityWoodElfKing.class, new RenderWoodElfKing());
+		RenderingRegistry.registerEntityRenderingHandler(EntityTiTun.class, new RenderTiTun());
+		RenderingRegistry.registerEntityRenderingHandler(EntityShadowWizard.class, new RenderBipedCore(RioVLib.essence, "Shadownite Wizard", Color.DARK_RED + "Raetiin"));
+		RenderingRegistry.registerEntityRenderingHandler(EntityRawDarkMatter.class, new RenderDarkMatter(new ResourceLocation(Util.MOD_ID + ":" + "darkMatter.png")));
+		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityNoFactionFlag.class, new RenderTileEntityNoFactionFlag());
+		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityRaetiinFlag.class, new RenderTileEntityRaetiinFlag());
+		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityJaerinFlag.class, new RenderTileEntityJaerinFlag());
+		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityInfuser.class, new RenderTileEntityInfuser());
+		RenderingRegistry.registerEntityRenderingHandler(EntityAltru.class, new RenderAltru());
+		MinecraftForge.EVENT_BUS.register(new GuiHelmetOverlay(Minecraft.getMinecraft()));
+		MinecraftForge.EVENT_BUS.register(new GuiEosBar(Minecraft.getMinecraft()));
+		MinecraftForge.EVENT_BUS.register(new GuiReputationMeter(Minecraft.getMinecraft()));
+
+		RioVAPI.getInstance().getLogger().info("HUD elements registered.");
+	}
+
+	@Override
+	public void registerStuff()
+	{
+		MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(RioVBlocks.noFacFlag), new ItemRenderNoFactionFlag());
+		MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(RioVBlocks.raetiinFlag), new ItemRenderRaetiinFlag());
+		MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(RioVBlocks.jaerinFlag), new ItemRenderJaerinFlag());
+
+		MinecraftForgeClient.registerItemRenderer(RioVItems.bowOfWurTun, new ItemRenderBow());
+		MinecraftForgeClient.registerItemRenderer(RioVItems.enforcedBowOfWurTun, new ItemRenderBow());
+		MinecraftForgeClient.registerItemRenderer(RioVItems.vraviniteBow, new ItemRenderLongbow());
+		RioVAPI.getInstance().getLogger().info("Item renderers registered.");
+		ExpansionChecker.checkNEI();
+	}
+
+	public static int addArmor(String armorName)
+	{
+		return RenderingRegistry.addNewArmourRendererPrefix(armorName);
+	}
+	
+	@Override
+	public ModelBiped getArmorModel(int id)
+	{
+		switch (id) {
+		case 0:
+			return dragonWings;
+		default:
+			break;
+		}
+		return dragonWings;
+	}
+}
